f0f64c0ee582da01d87639d4d5c10d534126a615
Harry <hjwp2@cantab.net>
2024-07-02 12:09:02 +0100

    first cut of download script with first team

--- download-all.sh
+++ download-all.sh
@@ -1,1 +1,10 @@
-# todo
+
+if [ -d team-3-FlexiSquid ]; then
+    # backup
+    mv team-3-FlexiSquid /tmp
+fi
+mkdir team-3-FlexiSquid
+curl -L https://github.com/KnightNight101/Komodo_Hackathon/archive/refs/heads/main.zip > team3.zip
+unzip team3.zip -d team-3-FlexiSquid
+rm team3.zip
+
