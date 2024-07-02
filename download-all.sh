101bc701bca49fc3a64aac3db69775771c9f3cd9
Harry <hjwp2@cantab.net>
2024-07-02 13:30:48 +0100

    bash script, make a function go on then

--- download-all.sh
+++ download-all.sh
@@ -1,21 +1,34 @@
+#!/bin/bash
+set -euo pipefail
 
-if [ -d FlexiSquid ]; then
-    # backup
-    mv FlexiSquid /tmp
-fi
-mkdir FlexiSquid
-curl -L https://github.com/KnightNight101/Komodo_Hackathon/archive/refs/heads/main.zip > FlexiSquid.zip
-unzip FlexiSquid.zip -d FlexiSquid
-rm FlexiSquid.zip
+function download {
+    local TEAM=$1
+    local REPO1=$2
+    echo "Team $TEAM"
 
-o
+    if [ "$#" -gt 2 ]; then
+        local REPO2=$3
+        mkdir -p "$TEAM/frontend"
+        curl -L "$REPO1/archive/refs/heads/main.zip" > "$TEAM-frontend.zip"
 
-if [ -d projectfeir ]; then
-    # backup
-    mv projectfeir /tmp
-fi
-mkdir projectfeir
-curl -L https://github.com/immo-huneke-zuhlke/projectfeir/archive/refs/heads/main.zip > projectfeir.zip
-unzip projectfeir.zip -d projectfeir
-rm projectfeir.zip
+        unzip "$TEAM-frontend.zip" -d "$TEAM/frontend"
+        rm "$TEAM-frontend.zip"
 
+        mkdir -p "$TEAM/backend"
+        curl -L "$REPO2/archive/refs/heads/main.zip" > "$TEAM-backend.zip"
+
+        unzip "$TEAM-backend.zip" -d "$TEAM/backend"
+        rm "$TEAM-backend.zip"
+
+    else
+        mkdir -p "$TEAM"
+        curl -L "$REPO1/archive/refs/heads/main.zip" > "$TEAM.zip"
+
+        unzip "$TEAM.zip" -d "$TEAM"
+        rm "$TEAM.zip"
+    fi
+}
+
+download "FlexiSquid" https://github.com/KnightNight101/Komodo_Hackathon
+download "FEIR" https://github.com/immo-huneke-zuhlke/projectfeir
+download "OctoWatt" https://github.com/octopus-game/octogame https://github.com/octopus-game/octopus-game-backend
